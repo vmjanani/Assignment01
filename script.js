0c7e52d83637bdf913f9e20b8dce3dc050a29844
vmjanani <vmjanani@gmail.com>
2023-09-21 14:15:15 -0700

    update script.js

--- script.js
+++ script.js
@@ -5,3 +5,21 @@
     // let currentMonth;
     // let summerMonth;
     // let myLibraryFunction;
+
+// STEP 02
+
+    //Example of numeric literal expression
+    
+    // 100
+    
+    //Example of a string literal expression
+    
+    // 'literal string expression'
+
+    //Example of a Boolean literal expression
+    
+    // true
+
+    //Example of a null literal expression
+
+    // null
