6f5cdbdd2cae3eb061a1a43736467f142d427873
vmjanani <vmjanani@gmail.com>
2023-09-21 14:28:17 -0700

    changes to script js

--- script.js
+++ script.js
@@ -23,3 +23,57 @@
     //Example of a null literal expression
 
     // null
+// STEP 03
+
+    // Two examples of completes / vaiable expressions
+
+    // let exampleOne = 8 / 2 + (6-1);
+    // let exampleTwo = variableA + variableB + variableC; 
+
+// STEP 04
+
+    // let strFirstName, strLastName, strAddress, strCity, strState, numZipCode, numYourAge, strReferralSource, boolMayWeContactYou;
+// STEP 05
+
+    // let strFirstName = window.prompt('What is your first name?');
+    // let numYourAge = 33;
+    // let boolMayWeContactYou = false;
+
+// STEP 06
+
+    // let total;
+    // total = 28 + " years";
+    // console.log(total);
+
+// STEP 07
+
+    // let name;
+    // name = true + ' janani';
+    // console.log(name);
+
+    // let name1;
+    // name1 = 'janani ' + false;
+    // console.log(name1);
+
+// STEP 08
+
+    // The example is wrong.  The apostrophe in "I'm" needs to be escaped .So we add a  backslash to indicate it's still part of the string and not the end of the intial quote.
+    // let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+    // console.log(someString);
+
+// STEP 09
+
+    // let nothing = null;
+    // console.log(nothing);
+
+// STEP 10
+
+    // let testString = 'string literal', testNum = 16, trueOrFalse = false, testObject = new function(){}, testUndefined;
+
+    // console.log(typeof testString, typeof testNum, typeof trueOrFalse, typeof testObject, typeof testUndefined);
+
+
+// STEP 11
+
+    // window.alert("Hello Zak Ruvalcaba ," + "welcome to the JavaScript class!"); 
+    // window.alert("Hello " + "Janani Varaha Moorthi" + ", welcome to the JavaScript class!");
